beeb3bcc45df373324d5ec38569d3fda121db6bf
Vera <vvegorova@gmail.com>
2022-01-12 21:49:42 +0100

    get_weather_forecast function

--- weather.py
+++ weather.py
@@ -1,7 +1,9 @@
 import logging
 import datetime
+import pytz
 import requests
 from pathlib import Path
+from typing import Dict
 
 
 def get_weather_key() -> str:
@@ -10,30 +12,33 @@ def get_weather_key() -> str:
     return weather_key
 
 
-get_timeline_url = "https://api.tomorrow.io/v4/timelines"
-weather_key = get_weather_key()
-location = [48.882272, 2.274652]
-fields = [
-  "precipitationIntensity",
-  "precipitationType",
-  "temperature",
-]
-units = "metric"
-timesteps = ["current", "1d"]
-now = datetime.datetime.now()
-startTime = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute, tzinfo=datetime.timezone.fromutc())
-end = now + datetime.timedelta(days=7)
-time_zone = "Europe/Paris"
-params = {"apikey": weather_key,
-          "location": f"{location[0]}, {location[1]}",
-          "units": units,
-          "fields": ",".join(fields),
-          "timesteps": ",".join(timesteps),
-          "timezone": time_zone,
-          "startTime": now.isoformat(),
-          "endTime": end.isoformat(),
-          }
-print(now.isoformat())
-request = requests.get(url=get_timeline_url, params=params)
+def get_weather_forecast() -> Dict:
+    location = [48.882272, 2.274652]
+    fields = [
+      "precipitationIntensity",
+      "precipitationType",
+      "temperature",
+    ]
 
-print(request.json())
+    time_zone = "Europe/Paris"
+    tz_info = pytz.timezone(time_zone)
+    now = datetime.datetime.now()
+    start_time = datetime.datetime(
+        now.year, now.month, now.day, now.hour, now.minute,
+        tzinfo=tz_info)
+    end = start_time + datetime.timedelta(days=1)
+
+    request = requests.get(
+        url="https://api.tomorrow.io/v4/timelines",
+        params={"apikey": get_weather_key(),
+                "location": f"{location[0]}, {location[1]}",
+                "units": "metric",
+                "fields": ",".join(fields),
+                "timesteps": "1h",
+                "timezone": time_zone,
+                "startTime": start_time.isoformat(),
+                "endTime": end.isoformat(),
+                })
+
+    return request.json()
+
